648ec17e6556759849c5967a1738a015ea211924
Peter Brightman <peter@brightman.de>
2023-03-23 18:10:47 +0100

    Add files
    Signed-off-by: Peter Brightman <peter@brightman.de>

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright company="brightman software studios" file="Program.cs">
+//   Copyright © brightman software studios 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Windows.Forms;
+
+namespace Fibonacci
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormMain());
+        }
+    }
+}
